6a650c40cdfbfc61d38f17f20ccb5f158cdc5528
milekboy <tijaniwahab25@gmail.com>
2025-07-24 10:18:21 +0100

    relocate public key in env file

--- src/app/pay-search/page.jsx
+++ src/app/pay-search/page.jsx
@@ -61,7 +61,7 @@ function ResultContent() {
   const reference = params.get("reference");
   const amount = params.get("amount");
 
-  const PUBLIC_KEY = "pk_test_8f50f7ec5175d60bdc0fd2645df800bd3dc37b49";
+  const PUBLIC_KEY = process.env.NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY;
 
   const handleConfirm = async ({ reference: ref }) => {
     setConfirming(true);
